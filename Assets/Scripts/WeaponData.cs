768dfc9d5620285f34bb960f58abffab50794a3b
whoiscadenyoung <cyoung5@trinity.edu>
2023-03-09 12:18:46 -0600

    asdf

--- Assets/Scripts/WeaponData.cs
+++ Assets/Scripts/WeaponData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 [CreateAssetMenu(menuName ="Items/Weapon")]
@@ -12,7 +13,14 @@ public class WeaponData : ItemData
     
     [Header("Weapon Settings")]
     public ToolType toolType;
+
+    public Animation lightAttack;
     public float attackDamage = 1f;
+    public float lightAttackDamage = 0.5f;
     public float attackSpeed = 1f;
     public float coolDown = 0.5f;
+
+    public void LightAttack()
+    {
+    }
 }
