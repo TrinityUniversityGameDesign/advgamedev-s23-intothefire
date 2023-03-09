6ead3e379e6c4511ef5d94241fbaeacb6dc2463c
jfroehli <jfroehli@trinity.edu>
2023-03-08 23:33:30 -0600

    movement is very unrefined, but the very broad strokes are there. Need to be cleaned up over spring break, but should be ok for testing purposes.

---
+++ Assets/Scripts/jacksonTestScripts/TempCameraController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TempCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target; // The object that the camera will follow
+    public float smoothSpeed = 0.125f; // The smoothing speed of the camera movement
+    public Vector3 offset; // The offset of the camera from the target
+    public float radius = 3f; // The distance of the camera from the target
+    public float cameraSensitivity = 4f; // The sensitivity of the camera movement
+
+    private float yaw = 0f;
+    private float pitch = 0f;
+
+    public bool lostFocus = true;
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            Gamepad gamepad = Gamepad.current;
+            if (gamepad != null)
+            {
+                Vector2 stickL = gamepad.rightStick.ReadValue();
+                yaw -= stickL.x;
+                pitch += stickL.y;
+            }
+
+            // Get mouse inputs for camera rotation
+            yaw -= Input.GetAxis("Mouse X") * cameraSensitivity;
+            pitch += Input.GetAxis("Mouse Y") * cameraSensitivity;
+
+
+            lostFocus = false;
+            // Limit pitch rotation
+            pitch = Mathf.Clamp(pitch, 10f, 80f);
+
+            // Convert spherical coordinates to Cartesian coordinates
+            float x = radius * Mathf.Sin(pitch * Mathf.Deg2Rad) * Mathf.Cos(yaw * Mathf.Deg2Rad);
+            float y = radius * Mathf.Cos(pitch * Mathf.Deg2Rad);
+            float z = radius * Mathf.Sin(pitch * Mathf.Deg2Rad) * Mathf.Sin(yaw * Mathf.Deg2Rad);
+
+            // Calculate the desired position of the camera
+            Vector3 desiredPosition = target.position + new Vector3(x, y, z);
+
+            // Smoothly move the camera to the desired position
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+            // Make the camera look at the target
+            transform.LookAt(target);
+        }
+        else
+        {
+            lostFocus = true;
+        }
+    }
+}
