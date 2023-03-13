df283d2f715c5469452b5ca24ae47529d7689f5f
TheNameIsGav <spqr.dominus@gmail.com>
2023-03-13 11:35:27 -0500

    Game timer and micro event selection timers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,9 @@ public enum GameState
     Showdown,
     Lobby,
     Paused,
-    GameOver
+    GameOver,
+    SideEvent,
+    EndScreen
 }
 
 public class GameManager : MonoBehaviour
@@ -22,16 +24,47 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     [Tooltip("Represents the number of fixed frames since the game has started.")]
-    public static float Timer; 
+    public static float Timer = 0;
+
+    [Tooltip("Represents the number of minutes the game should last")]
+    public float Minutes = 5;
 
     [Tooltip("A demo event used to demonstrate how events can be used.")]
     public static UnityEvent DemoEvent;
+
+    [Tooltip("Event used to call when a Side Event is starting")]
+    public static UnityEvent SideEventBegin;
+
+    [Tooltip("Event used to indicate when the Labyrinth is beginning to be explored")]
+    public static UnityEvent LabyrinthExploreBegin;
     #endregion
 
     #region Private Fields
     [Tooltip("Internal state the game manager is currently in.")]
     private GameState _state;
+
     #endregion
+
+    #region Event Fields
+    //Timing controls between events
+    public float MaxSecondsBetweenSideEvents = 600;
+    public float MinSecondsBetweenSideEvents = 300;
+
+    public float MaxSecondsBetweenMicroEvents = 180;
+    public float MinSecondsBetweenMicroEvents = 60;
+
+    //Side events are fixed duration
+    public float MaxSecondsDurationMicroEvents = 20;
+    public float MinSecondsDurationMicroEvents = 10;
+
+    float timeUntilNextMicroEvent;
+    float timeLeftInMicroEvent;
+
+    float timeUntilNextSideEvent;
+    float timeLeftInSideEvent;
+
+    bool microEventInProgress = false; 
+    #endregion 
 
     #region Private Methods
     private void Start()
@@ -39,35 +72,66 @@ public class GameManager : MonoBehaviour
         Instance = this;
 
         if (DemoEvent == null) DemoEvent = new UnityEvent();
-        DemoEvent.AddListener(Ping);
+        DemoEvent.AddListener(TestDemoEvent);
+
+        if (SideEventBegin == null) SideEventBegin = new UnityEvent();
+        SideEventBegin.AddListener(TestSideEventBegin);
+
+        if (LabyrinthExploreBegin == null) LabyrinthExploreBegin = new UnityEvent();
+        LabyrinthExploreBegin.AddListener(TestLabyrinthBegin);
+
+        OnStateEnter(GameState.Lobby);
+        
     }
 
     private void FixedUpdate()
     {
         TickState();
 
-        if (Input.GetKeyDown(KeyCode.P))
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightControl)) //Activate Debug KeyCommands
         {
-            DemoEvent?.Invoke();
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                OnStateEnter(GameState.Labyrinth_Explore);
+            }
         }
     }
 
     private void OnStateEnter(GameState nextState)
     {
+        //Exit the current state, then call the enter on the nextState.
         OnStateExit();
         _state = nextState;
         switch (_state)
         {
             case GameState.GameOver:
+                Timer = 0;
                 break;
             case GameState.Labyrinth_Explore:
-                Timer = 0;
+                Debug.Log("Entering Labyrinth_Explore");
+
+                LabyrinthExploreBegin?.Invoke();
+
+                timeUntilNextMicroEvent = Random.Range(MinSecondsBetweenMicroEvents, MaxSecondsBetweenMicroEvents);
+                timeUntilNextSideEvent = Random.Range(MinSecondsBetweenSideEvents, MaxSecondsBetweenSideEvents);
+
+                Debug.Log("Time until next Micro Event: " + timeUntilNextMicroEvent + " s");
                 break;
             case GameState.Lobby:
                 break;
             case GameState.Paused:
                 break;
             case GameState.Showdown:
+                Debug.Log("Entering Showdown");
+                break;
+            case GameState.SideEvent:
+                SideEventBegin?.Invoke();
+                break;
+            case GameState.EndScreen:
                 break;
             default:
                 break;
@@ -81,13 +145,37 @@ public class GameManager : MonoBehaviour
             case GameState.GameOver:
                 break;
             case GameState.Labyrinth_Explore:
-                Timer++;
+                Timer += Time.deltaTime;
+
+                if(!microEventInProgress) timeUntilNextMicroEvent -= Time.deltaTime;
+                if (microEventInProgress) timeLeftInMicroEvent -= Time.deltaTime;
+
+
+                if(timeUntilNextMicroEvent <= 0 && !microEventInProgress)
+                {
+                    BeginMicroEvent();
+                }
+
+                if(microEventInProgress && timeLeftInMicroEvent <= 0)
+                {
+                    EndMicroEvent();
+                }
+
+                if(Timer >= Minutes * 60)
+                {
+                    OnStateEnter(GameState.Showdown);
+                }
+
                 break;
             case GameState.Lobby:
                 break;
             case GameState.Paused:
                 break;
             case GameState.Showdown:
+                break;
+            case GameState.SideEvent:
+                break;
+            case GameState.EndScreen:
                 break;
             default:
                 break;
@@ -108,14 +196,46 @@ public class GameManager : MonoBehaviour
                 break;
             case GameState.Showdown:
                 break;
+            case GameState.SideEvent:
+                break;
+            case GameState.EndScreen:
+                break;
             default:
                 break;
         }
     }
 
-    void Ping()
+    void TestDemoEvent()
     {
-        Debug.Log("Ping");
+        Debug.Log("TestDemoEvent");
+    }
+
+    void TestSideEventBegin()
+    {
+        Debug.Log("SideEventBegin");
+    }
+
+    void TestLabyrinthBegin()
+    {
+        Debug.Log("LabyrinthBegin");
+    }
+
+    void BeginMicroEvent()
+    {
+        microEventInProgress = true;
+        Debug.Log("Beginning Microevent at " + Timer + " game seconds.");
+
+        timeLeftInMicroEvent = Random.Range(MinSecondsDurationMicroEvents, MaxSecondsDurationMicroEvents);
+        Debug.Log("Microevent Duration: " + timeLeftInMicroEvent + " s");
+    }
+
+    void EndMicroEvent()
+    {
+        microEventInProgress = false;
+        Debug.Log("Ending Microevent at " + Timer + " game seconds.");
+
+        timeUntilNextMicroEvent = Random.Range(MinSecondsBetweenMicroEvents, MaxSecondsBetweenMicroEvents);
+        Debug.Log("Time until next Micro Event: " + timeUntilNextMicroEvent + " s");
     }
 
     #endregion
