0c7e8a5e296fdf9f0b73d59a6751cf28fc541094
jfroehli <jfroehli@trinity.edu>
2023-03-20 22:03:05 -0500

    shit got really fucked but movement has been merged into purple

--- Assets/Scripts/jacksonTestScripts/JacksonNoDestroy.cs
+++ Assets/Scripts/jacksonTestScripts/JacksonNoDestroy.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestroyOnLoadScript : MonoBehaviour
+public class JacksonNoDestroy : MonoBehaviour
 {
+	// Start is called before the first frame update
 	private void Awake()
 	{
 		DontDestroyOnLoad(transform.gameObject);
