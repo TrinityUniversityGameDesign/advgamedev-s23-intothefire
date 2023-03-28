bebf30440ea47b7d34eeef38d3c4cb1e6a46744b
whoiscadenyoung <cyoung5@trinity.edu>
2023-03-28 11:55:21 -0500

    basic hud and minimap
    minimap and hud are separate test scenes. Nothing is fully implemented yet

---
+++ Assets/Scripts/LerpColor.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))] 
+public class LerpColor : MonoBehaviour
+{
+    public Color color1;
+    public Color color2;
+    
+    private Image _image;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    /// <summary>
+    /// Updates color of image based on slider value?
+    /// </summary>
+    /// <param name="value">Should be a dynamic float pulled from slider...</param>
+    public void OnSliderValueChanged(float value)
+    {
+        Debug.Log(value);
+        Color lerpColor = Color.Lerp(color1, color2, value);
+        _image.color = lerpColor;
+    }
+}
