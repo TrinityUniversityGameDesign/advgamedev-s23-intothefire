bbe9bf932cef30de0b8928666b632e09308870b5
Guwahavel <andrewangaroni@outlook.com>
2023-04-17 16:16:13 -0500

    Projectile disipate on terrain contact

--- Assets/Scripts/EnemyScripts/EnemyHitbox.cs
+++ Assets/Scripts/EnemyScripts/EnemyHitbox.cs
@@ -6,14 +6,21 @@ public class EnemyHitbox : MonoBehaviour
 {
     public int duration;
     public bool isProjectile;
+    public GameObject terrainCollider;
     Vector3 velocity = Vector3.zero;
 
-    void FixedUpdate()
-    {
+    void Start() {
+        terrainCollider = transform.Find("TerrainCollider").gameObject;
+    }
+
+    void FixedUpdate() {  
         --duration;
         if (duration <= 0) {Destroy(gameObject);}
-
         transform.position += velocity;
+        if (isProjectile) {
+            terrainCollider.transform.localScale = transform.localScale;
+            terrainCollider.GetComponent<Collider>().isTrigger = false;
+        }
     }
 
     public void SetDuration(int i) {duration = i;}
@@ -21,8 +28,4 @@ public class EnemyHitbox : MonoBehaviour
     public void SetDamage(float f) {GetComponent<DamageScript>().SetDamage(f);}
     public void SetKnockback(float f) {GetComponent<DamageScript>().SetKnockback(f);}
     public void SetVelocity(Vector3 v) {velocity = v;}
-
-    private void OnTriggerEnter(Collider other) {
-		if (isProjectile) {Destroy(gameObject);}
-	}
 }
