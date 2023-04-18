01662ea62295678b157a764509245c2317df720d
DESKTOP-D4A1412\larry <lgreen3@trinity.edu>
2023-04-18 11:44:15 -0500

    Ax Modified

--- Assets/Scripts/AxSwing.cs
+++ Assets/Scripts/AxSwing.cs
@@ -24,7 +24,6 @@ public class AxSwing : MonoBehaviour
         if (Mathf.Abs(newAngle) >= swayAngle) {
             isSwayingRight = !isSwayingRight;
         }
-        Debug.Log("Ax position: " + axis.transform.position);
     }
 
     
