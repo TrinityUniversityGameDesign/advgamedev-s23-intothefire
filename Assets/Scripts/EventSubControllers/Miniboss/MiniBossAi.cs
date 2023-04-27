408b8ad761c554288e4f25576dd6e1c47d365e94
TheNameIsGav <spqr.dominus@gmail.com>
2023-04-26 22:40:35 -0500

    Added unkeyed stomp and charge attacks

--- Assets/Scripts/EventSubControllers/Miniboss/MiniBossAi.cs
+++ Assets/Scripts/EventSubControllers/Miniboss/MiniBossAi.cs
@@ -10,6 +10,7 @@ public class MiniBossAi : MonoBehaviour
     public float speed;
     public float range; // the range within which to choose a random point
     public float angularSpeed;
+    public int UniqueAttackCooldown = 5;
 
     private NavMeshAgent navAgent; // reference to the NavMeshAgent component
     Animator _anim;
@@ -21,7 +22,8 @@ public class MiniBossAi : MonoBehaviour
 
     List<float> damageTracker;
 
-    bool windingUpAttack = false;
+    bool canAttack = true;
+    bool attacking = false;
 
     // Start is called before the first frame update
     void Start()
@@ -43,57 +45,94 @@ public class MiniBossAi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If I am at the navigation point, then see if can attack
+        //If I can, then lets trigger the attack. 
+
+
         // if the NavMeshAgent has reached its destination, set a new random destination
-        if (navAgent.remainingDistance <= navAgent.stoppingDistance && !windingUpAttack)
+        if (navAgent.remainingDistance <= navAgent.stoppingDistance && !attacking)
         {
-            //See if we should make a new attack
-            
-            if(!CheckRandomAttack()) SetRandomDestination();
+            if (Random.Range(0f, 1f) <= .25)
+            {
+                SelectRandomAttack();
+            } else
+            {
+                SetRandomDestination();
+            }
         }
     }
 
-    bool CheckRandomAttack()
+    IEnumerator AttackCooldown(int delay)
     {
+        attacking = true;
+        yield return new WaitForSeconds(delay);
+        canAttack = true;
+        attacking = false;
+        navAgent.speed = speed;
+        SetRandomDestination();
+
+        base_damage.SetActive(true);
+        charge.SetActive(false);
+        jump.SetActive(false);
+        stomp.SetActive(false);
+    }
+
+    //Returns false if we cant make a new attack
+    void SelectRandomAttack()
+    {
+        base_damage.SetActive(false);
         //Lets see if we should make a new attack and then do that. 
         int attack = Random.Range(0, 3);
-
         switch (attack)
         {
             case 0:
-                windingUpAttack = true;
                 PerformStompAttack();
-                return true;
-            case 1:
-                windingUpAttack = true;
-                PerformJumpAttack();
-                return true;
+                break;
+            //case 1:
+            //    PerformJumpAttack();
+            //    break;
             case 2:
-                windingUpAttack = true;
                 PerformChargeAttack();
-                return true;
+                break;
             case 3:
                 break;
             default:
-                windingUpAttack = false;
                 break;
         }
-
-        return false;
     }
 
-    private void PerformStompAttack()
+    private IEnumerator PerformStompAttack()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("Stomp Attack Triggered");
+        stomp.SetActive(true);
+        canAttack = false;
+        return AttackCooldown(5);
     }
 
-    private void PerformJumpAttack()
+    private IEnumerator PerformJumpAttack()
     {
-        throw new System.NotImplementedException();
+        
+        canAttack = false;
+        Debug.Log("Jump Attack Triggered");
+        return AttackCooldown(4);
     }
 
-    private void PerformChargeAttack()
+    private IEnumerator PerformChargeAttack()
     {
-        throw new System.NotImplementedException();
+        charge.SetActive(true);
+        canAttack = false;
+        Debug.Log("Charge Triggered");
+        RaycastHit hit;
+        Physics.Raycast(new Ray(transform.position, transform.forward * 100), out hit);
+
+        NavMeshHit navHit;
+        if (NavMesh.SamplePosition(hit.point, out navHit, 50, NavMesh.AllAreas))
+        {
+            navAgent.SetDestination(navHit.position);
+            navAgent.speed = speed * 10;
+        }
+
+        return AttackCooldown(3);
     }
 
     // sets a random destination within the range
