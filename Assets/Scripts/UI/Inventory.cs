634b99be834863c3bdf607b9805565c045742970
whoiscadenyoung <cyoung5@trinity.edu>
2023-04-28 18:34:35 -0500

    updated some prefabs to get everything mostly working

--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -15,7 +15,10 @@ public class Inventory : MonoBehaviour
         for (int i = 0; i < items.Count; i++)
         {
             GameObject newObject = Instantiate(itemPrefab, transform);
-            newObject.gameObject.GetComponentInChildren<ImageController>().SetImageToSprite(items[i].icon);
+            Debug.Log("Made object from prefab: " + newObject.name);
+            var imageComponent = newObject.gameObject.GetComponentInChildren<ImageController>();
+            Debug.Log("Got the image component: " + (imageComponent != null));
+            imageComponent.SetImageToSprite(items[i].icon);
             newObject.transform.Find("name").GetComponent<TMP_Text>().text = items[i].name;
             newObject.transform.Find("desc").GetComponent<TMP_Text>().text = items[i].description;
         }
